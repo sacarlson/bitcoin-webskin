dc16d52c5b4a3f00370f70f29652a21ac5297435
zamgo <sales@2meta.com>
2011-06-07 03:22:47 -0700

    example config

--- config.sample.php
+++ config.sample.php
@@ -4,43 +4,18 @@
 	Copyright (c) 2011 14STzHS8qjsDPtqpQgcnwWpTaSHadgEewS
 */
 
-// RPC/JSON access:
+// Communicate with bitcoind via JSON RPC calls:
 
 define('USERNAME', 	'test');
 define('PASSWORD', 	'test');
-define('SCHEME',	'http');
-define('HOST',     	'127.0.0.1');
-define('PORT',     	'8332');
-
-// Interface setup:
-
-//define('TUBE', 		'test');
-
-define('TUBE', 		'bitcoin-php'); 
-define('CERTIFICATE_PATH', '');
+define('SCHEME',	'http');        // http  or https  
+define('HOST',     	'127.0.0.1');   
+define('PORT',     	'8332');        // Bitcoin standard port: 8332
+define('SERVER_NETWORK','Bitcoin');     // Display name of the network
 
 
-define('SERVER',         		'C:\bitcoind.exe');
-define('SERVER_NAME',    		'bitcoind.exe');
-define('SERVER_NETWORK',    	'Bitcoin');
-define('SERVER_TESTNET',    	true);
-define('SERVER_DATADIR', 		'C:\bitcoind-datadir');
-define('SERVER_CONF',    		'C:\bitcoind-datadir\sample.conf');
+// Interface for bitcoin protocol:
 
+define('TUBE', 		'bitcoin-php'); // use bitcoin-php library plugin
+define('CERTIFICATE_PATH', '');         // used for bitcoin-php library
 
-/*
-define('SERVER',         		'C:\namecoind.exe');
-define('SERVER_NAME',    		'namecoind.exe');
-define('SERVER_NETWORK',    	'Namecoin');
-define('SERVER_TESTNET',    	true);
-define('SERVER_DATADIR', 		'C:\namecoind-datadir');
-define('SERVER_CONF',    		'C:\namecoind-datadir\sample.conf');
-*/
-
-
-
-// Localhost Server
-
-define('SERVER_LOCALHOST', 		true);
-define('SERVER_LOCALHOST_TYPE', 'windows');
-define('WINDOWS_TASKLIST', 		'C:\Windows\System32\tasklist.exe');

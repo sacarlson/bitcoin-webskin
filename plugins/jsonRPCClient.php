fc8288fe491efbe119953f452060c1015c1b8462
Zamgo <sales@2meta.com>
2011-06-22 23:24:55 +0200

    customized, smaller jsonRPCClient

--- plugins/jsonRPCClient.php
+++ plugins/jsonRPCClient.php
@@ -357,9 +357,8 @@ class jsonRPCClientControler implements Bitcoin, Namecoin {
 ?><?php
 
 /*
-	Modified for Bitcoin Webskin
+	jsonRPCClient - Modified for Bitcoin Webskin
 */
-
 /*
 					COPYRIGHT
 
@@ -381,165 +380,60 @@ You should have received a copy of the GNU General Public License
 along with JSON-RPC PHP; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
 /**
  * The object of this class are generic jsonRPC 1.0 clients
  * http://json-rpc.org/wiki/specification
  *
  * @author sergio <jsonrpcphp@inservibile.org>
  */
+ 
 class jsonRPCClient {
 	
-	/**
-	 * Debug state
-	 *
-	 * @var boolean
-	 */
-	private $debug;
-	
-	/**
-	 * The server URL
-	 *
-	 * @var string
-	 */
-	private $url;
-	/**
-	 * The request id
-	 *
-	 * @var integer
-	 */
-	private $id;
-	/**
-	 * If true, notifications are performed instead of requests
-	 *
-	 * @var boolean
-	 */
-	private $notification = false;
-	
-	/**
-	 * Takes the connection parameters
-	 *
-	 * @param string $url
-	 * @param boolean $debug
-	 */
-	public function __construct($url,$debug = false) {
-		// server URL
-		$this->url = $url;
-		// proxy
-		empty($proxy) ? $this->proxy = '' : $this->proxy = $proxy;
-		// debug state
-		empty($debug) ? $this->debug = false : $this->debug = true;
-		// message id
-		$this->id = 1;
-	}
-	
-	/**
-	 * Sets the notification state of the object. In this state, notifications are performed, instead of requests.
-	 *
-	 * @param boolean $notification
-	 */
-	public function setRPCNotification($notification) {
-		empty($notification) ?
-							$this->notification = false
-							:
-							$this->notification = true;
-	}
-	
-	/**
-	 * Performs a jsonRCP request and gets the results as an array
-	 *
-	 * @param string $method
-	 * @param array $params
-	 * @return array
-	 */
-	public function __call($method,$params) {
-		
-		//$this->debug("Call: $method ");
-		//if( $params ) { $this->debug($params); }
-		
-		// check
-		if (!is_scalar($method)) {
-			throw new Exception('Method name has no scalar value');
-		}
-		
-		// check
-		if (is_array($params)) {
-			// no keys
-			$params = array_values($params);
-		} else {
-			throw new Exception('Params must be given as array');
-		}
-		
-		// sets notification or request task
-		if ($this->notification) {
-			$currentId = NULL;
-		} else {
-			$currentId = $this->id;
-		}
-		
-		// prepares the request
-		$request = array(
-						'method' => $method,
-						'params' => $params,
-						'id' => $currentId
-						);
-						
-		
-		$request = json_encode($request);
+	private $url, $debug;
 
+	public function __construct( $url, $debug = false ) {
+		$this->url = $url;
+		$this->debug = $debug;
+	}
+	
+	public function __call( $method, $params ) {
+	
+		if( !is_scalar($method) ) { throw new Exception('Method name has no scalar value'); }
+		if( !is_array($params)  ) { throw new Exception('Params must be given as array');   }
+		
+		$request = json_encode( array( 'method' => $method, 'params' => array_values($params), 'id' => 2 ) );
 		$this->debug("&gt; $request");
 		
-		// performs the HTTP POST
-		$opts = array ( 'http' => array (
-			'method'  => 'POST',
-			'header'  => 'Content-type: application/json',
-			'timeout'  => 5,
-			'ignore_errors' => 1,
-			'content' => $request )
-		);
-		$context  = stream_context_create($opts);
-		
+		$opts = array ( 'http' => array ( 'method'  => 'POST', 'header'  => 'Content-type: application/json',
+			'timeout'  => 5, 'ignore_errors' => 1, 'content' => $request ) );
 
-		// made @fopen silent errors ... 
-		if ( $fp = @fopen($this->url, 'r', false, $context) ) {
+		if( $fp = @fopen($this->url, 'r', false, stream_context_create($opts) ) ) {
 			$response = '';
-			while($row = fgets($fp)) {
-				$response.= trim($row)."\n";
-			}
-			
+			while($row = fgets($fp)) { $response .= trim($row) . "\n"; }
 			$this->debug("&lt $response");
-			
 			$response = json_decode($response,true);
 		} else {
-
 			$this->debug('Error: unable to connect to wallet');
 			throw new Exception("Unable to connect to wallet.");
 		}
 		
-
-		
-		// final checks and return
-		if (!$this->notification) {
-			// check
-			if ($response['id'] != $currentId) {
-				throw new Exception('Incorrect response id (request id: '.$currentId.', response id: '.$response['id'].')');
-			}
-			if (!is_null($response['error'])) {
-                               $this->debug('Error ' . $response['error']['code'] . ' : ' . $response['error']['message'] );
-                                $response['result'] = $response['error'];
-			}
-			
-			return $response['result'];
-			
-		} else {
-			return true;
+		if( $response['id'] != 2 ) {
+			$this->debug('Error: Incorrect response id from jsonRPC call');
+			//throw new Exception('Incorrect response id (request id: '.$currentId.', response id: '.$response['id'].')');
 		}
+	
+		if( !is_null($response['error']) ) {
+			$this->debug('Error ' . $response['error']['code'] . ' : ' . $response['error']['message'] );
+			$response['result'] = $response['error'];
+		}
+			
+		return $response['result'];
+
 	}
 
 	public function debug($msg) {
 		if( !$this->debug ) { return; }
 		print "<pre style='margin:0'>DEBUG: "; print_r($msg); print '</pre>';
-		//print "<span style='margin:0'><br />DEBUG: "; print_r($msg); print '</span>';
 	}	
 	
 }
